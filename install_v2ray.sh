08034e2eaab39cd230c6bc13cb22b53cc7ec2cd7
root <qiqitao320@qq.com>
2026-01-12 21:35:09 +0800

    chmod +x

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -18,7 +18,7 @@ cp config.json /usr/local/etc/v2ray/config.json
 cp geoip.dat /usr/local/share/v2ray/geoip.dat
 cp v2ray /usr/local/bin/v2ray
 cp v2ray.service /usr/lib/systemd/system/v2ray.service
-
+chmod +x /usr/local/bin/v2ray
 
 systemctl enable --now v2ray
 systemctl status v2ray
